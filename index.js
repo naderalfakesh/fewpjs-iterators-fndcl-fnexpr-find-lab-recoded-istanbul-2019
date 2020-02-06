6c4164130ec02ec7f240ad385052d5a2d9c6d6ea
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-06 04:01:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function testFunc() {
 }
 
 function superbowlWin(arr){
-    const res =  arr.find(function(element){console.log(element["result"] === "W");return element["result"] === "W" });
+    const res =  arr.find(function(element){return element["result"] === "W" });
     console.log(res)
     res != undefined ? res["year"] : res;
 }
